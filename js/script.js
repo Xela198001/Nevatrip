41eb554ad01eba8ae7950a59ea9c63aa7dc93f50
Meshcheriakov Aleksandr <mes-aleksandr@yandex.ru>
2021-05-17 13:50:32 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
 window.onload = function (event) {
-
   const flightTimeHideShow = (count, item, disp) => {
-
-    
-    console.log(count)
+    console.log(count);
 
     if (count > 4) {
       for (let idx = 4; idx < count; idx++) {
         item.children[idx].style.display = disp;
       }
     }
-  }
+  };
   let flightTime = document.querySelectorAll(".flight-time");
   flightTime.forEach((item) => {
     var count = item.children.length;
-    
-     flightTimeHideShow(count, item, "none");
+
+    flightTimeHideShow(count, item, "none");
 
     if (count > 4) {
-      const more = document.createElement('div');
-     more.classList.add('time-item');
-     more.innerHTML = "еще";
-     item.appendChild(more);
-     more.addEventListener("click", () => {
-       flightTimeHideShow(count, item, "flex");
-       more.remove();
-     }, false);}
+      const more = document.createElement("div");
+      more.classList.add("time-item");
+      more.innerHTML = "еще";
+      item.appendChild(more);
+      more.addEventListener(
+        "click",
+        () => {
+          flightTimeHideShow(count, item, "flex");
+          more.remove();
+        },
+        false
+      );
+    }
   });
-}
+};
